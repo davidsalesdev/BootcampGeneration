7f78fea20ab1ff8a7ca83347248d75314a00cdf8
David <b4rbab4rba@gmail.com>
2022-06-11 20:06:37 -0300

    Ajustes

--- BlogAngular/blogPessoal-api-master/src/main/java/generation/org/blogPessoal/model/Postagem.java
+++ BlogAngular/blogPessoal-api-master/src/main/java/generation/org/blogPessoal/model/Postagem.java
@@ -10,7 +10,6 @@ import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
-import javax.validation.constraints.Size;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.sun.istack.NotNull;
@@ -19,25 +18,23 @@ import com.sun.istack.NotNull;
 @Table(name = "postagem")
 public class Postagem {
 
-	@Id	
+	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long id;
 
 	@NotNull
-	@Size(min = 5, max = 100)
 	private String titulo;
-	
+
 	@NotNull
-	@Size(min = 10, max = 500)
 	private String texto;
 
 	@Temporal(TemporalType.TIMESTAMP)
-    private Date data = new java.sql.Date(System.currentTimeMillis());
-	
+	private Date data = new java.sql.Date(System.currentTimeMillis());
+
 	@ManyToOne
 	@JsonIgnoreProperties({"postagem"})
 	private Tema tema;
-}
+
 
 	public long getId() {
 		return id;
--- BlogAngular/blogPessoal-api-master/src/main/java/generation/org/blogPessoal/model/Usuario.java
+++ BlogAngular/blogPessoal-api-master/src/main/java/generation/org/blogPessoal/model/Usuario.java
@@ -1,12 +1,13 @@
 package generation.org.blogPessoal.model;
 
+import com.sun.istack.NotNull;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
+
 
 @Entity
 @Table(name = "usuarios")
@@ -17,15 +18,15 @@ public class Usuario {
 	private long id;
 	
 	@NotNull
-	@Size(min = 5, max = 100)
+
 	private String nome;
 	
 	@NotNull
-	@Size(min = 5, max = 100)
+
 	private String usuario;
 	
 	@NotNull
-	@Size(min = 5)
+
 	private String senha;
 	
 
